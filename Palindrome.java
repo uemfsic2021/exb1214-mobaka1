22cd38c0c04800841dadcb070c1c3051eb99eaf8
mobaka1 <>
2021-04-07 13:50:10 +0000

    bonsoir, voici mon code

--- Palindrome.java
+++ Palindrome.java
@@ -1,7 +1,19 @@
+import javax.lang.model.util.ElementScanner6;
+
 public class Palindrome{
     public static boolean is_palindrome(String s)
     {
-        // add your code here
-        return true;
+       int a = (s.length()-1);
+       int b = 0;
+       while(b < a && s.charAt(b) == s.charAt(a)) {
+        b+=1;
+        a-=1;
+       }
+       if(s.charAt(b)!=s.charAt(a)) {
+         return false;}
+         else {
+              return true;
+              }
+          
     }
 }
